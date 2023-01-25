ed58bee935e7095fc1b49739e3fe14ade6b3df70
Rafael Wainer <rafael.wainer@gmail.com>
2023-01-25 19:53:28 -0300

    lambda stop and start ecs service

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,13 +1,59 @@
 import json
 import boto3
 
-s3Client = boto3.client('s3')
 cluster_name = "arn:aws:ecs:us-east-1:266070530669:cluster/cluster-awake-and-shut-down"
 
 
+def session_boto3():
+
+    with open("credentials/credentials.json", encoding='utf-8') as arquivo_json:
+        dados = json.load(arquivo_json)
+
+        # Para consultar credentials + session token, utilizar codigo abaixo com o aws cli instalado
+        # aws sts get-session-token --duration-seconds 129600
+        aws_region = dados['aws_region']
+        aws_access_id = dados['aws_access_key_id']
+        aws_secret_key = dados['aws_secret_access_key']
+        aws_access_token = dados['aws_access_token']
+
+        return boto3.Session(
+            aws_access_key_id=aws_access_id,
+            aws_secret_access_key=aws_secret_key,
+            aws_session_token=aws_access_token,
+            region_name=aws_region
+        )
+
+
+# Sets the desired count of tasks per service to 1
+# Container will spawn after a few moments
+def spawncontainer(servicearns, cluster_name):
+    client = boto3.client('ecs')
+    for srv in servicearns:
+        client.update_service(
+            cluster=cluster_name,
+            service=srv,
+            desiredCount=2,
+        )
+
+
+# Sets the desired count of tasks per service to 0
+# Services still runs but without any container
+def stopcontainer(servicearns, cluster_name):
+    client = boto3.client('ecs')
+    for srv in servicearns:
+        client.update_service(
+            cluster=cluster_name,
+            service=srv,
+            desiredCount=0,
+        )
+
+
 def lambda_handler(event, context):
-    print(f"Event -> {event}")
+    print(f"\nEvent -> {event}")
     print(f"\nContext -> {context}")
+
+    session = session_boto3()
+    session.client('s3')
 
     # print(f"Vou buscar action")
     # cloudwatchvalue = event['action']
@@ -17,27 +63,33 @@ def lambda_handler(event, context):
     # cluster_name = event['cluster']
     # print(f"Peguei cluster {cluster_name}")
 
-    print(f"Vou conectar no client ecs")
+    print(f"\nVou conectar no client ecs")
     client = boto3.client('ecs')
-    print(f"Me conectei com sucesso no ecs")
+    print(f"\nMe conectei com sucesso no ecs")
 
     try:
         response = client.list_services(
             cluster=cluster_name,
-            launchType="FARGATE",
+            launchType="FARGATE"
             # maxResults=2,
             # schedulingStrategy="REPLICA"
         )
-        print(f"Sucesso no response do list_tasks => {response['serviceArns']}")
+        print(f"\nSucesso no response do list_tasks => {response['serviceArns']}")
     except:
-        print(f"Didnt work Cluster = {cluster_name}")
+        print(f"\nDidnt work Cluster = {cluster_name}")
 
     service_list = response['serviceArns']
 
     start_task = "rafawainer-s3-lambda-starter"
-    print(f"Start Key = {start_task}")
+    print(f"\nStart Key = {start_task}")
     stop_task = "rafawainer-s3-lambda-stopper"
     print(f"Stop Key = {stop_task}")
+
+    print(f"\nEvent Records -> {event['Records']}")
+    print(f"Event Records[s3] -> {event['Records'][0]['s3']}")
+    print(f"Event Records[s3][bucket] -> {event['Records'][0]['s3']['bucket']}")
+    print(f"Event Records[s3][bucket][name\ -> {event['Records'][0]['s3']['bucket']['name']}")
+
     s3_bucket_key = event['Records'][0]['s3']['bucket']['name']
     print(f"S3 Bucket Key = {s3_bucket_key}")
 
@@ -45,40 +97,16 @@ def lambda_handler(event, context):
     # if 'start' == cloudwatchvalue:
     if start_task == s3_bucket_key:
         spawncontainer(service_list, cluster_name)
-        print(f"Start spwncontainer com sucesso")
+        print(f"\nStart spwncontainer com sucesso")
 
     # stop service with desired tasks
     # elif 'stop' == cloudwatchvalue:
     elif stop_task == s3_bucket_key:
         stopcontainer(service_list, cluster_name)
-        print(f"Stop stopcontainer com sucesso")
+        print(f"\nStop stopcontainer com sucesso")
 
     return {
         'statusCode': 200,
         'body': json.dumps('Script finished')
 
     }
-
-
-### Sets the desired count of tasks per service to 1
-### Container will spawn after a few moments
-def spawncontainer(servicearns, clusterName):
-    client = boto3.client('ecs')
-    for srv in servicearns:
-        responseUpdate = client.update_service(
-            cluster=clusterName,
-            service=srv,
-            desiredCount=2,
-        )
-
-
-### Sets the desired count of tasks per service to 0
-### Services still runs but without any container
-def stopcontainer(servicearns, clusterName):
-    client = boto3.client('ecs')
-    for srv in servicearns:
-        responseUpdate = client.update_service(
-            cluster=clusterName,
-            service=srv,
-            desiredCount=0,
-        )
