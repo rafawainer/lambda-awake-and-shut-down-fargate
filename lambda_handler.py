d5f294c174f673016780e22a974c6fed4cf48a85
Rafael Wainer <rafael.wainer@gmail.com>
2023-01-25 18:02:48 -0300

    lambda stop and start ecs service

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,17 +1,21 @@
 import json
 import boto3
 
+s3Client = boto3.client('s3')
+cluster_name = "arn:aws:ecs:us-east-1:266070530669:cluster/cluster-awake-and-shut-down"
+
+
 def lambda_handler(event, context):
     print(f"Event -> {event}")
     print(f"\nContext -> {context}")
 
-    print(f"Vou buscar action")
-    cloudwatchvalue = event['action']
-    print(f"Peguei action {cloudwatchvalue}")
+    # print(f"Vou buscar action")
+    # cloudwatchvalue = event['action']
+    # print(f"Peguei action {cloudwatchvalue}")
 
-    print(f"Vou buscar cluster")
-    cluster_name = event['cluster']
-    print(f"Peguei cluster {cluster_name}")
+    # print(f"Vou buscar cluster")
+    # cluster_name = event['cluster']
+    # print(f"Peguei cluster {cluster_name}")
 
     print(f"Vou conectar no client ecs")
     client = boto3.client('ecs')
@@ -26,15 +30,26 @@ def lambda_handler(event, context):
         )
         print(f"Sucesso no response do list_tasks => {response['serviceArns']}")
     except:
-        print(f"Didnt work Cluster = {cluster_name} / Event {cloudwatchvalue}")
+        print(f"Didnt work Cluster = {cluster_name}")
 
     service_list = response['serviceArns']
 
-    if 'start' == cloudwatchvalue:
+    start_task = "rafawainer-s3-lambda-starter"
+    print(f"Start Key = {start_task}")
+    stop_task = "rafawainer-s3-lambda-stopper"
+    print(f"Stop Key = {stop_task}")
+    s3_bucket_key = event['Records'][0]['s3']['bucket']['name']
+    print(f"S3 Bucket Key = {s3_bucket_key}")
+
+    # start service with desired tasks
+    # if 'start' == cloudwatchvalue:
+    if start_task == s3_bucket_key:
         spawncontainer(service_list, cluster_name)
         print(f"Start spwncontainer com sucesso")
 
-    elif 'stop' == cloudwatchvalue:
+    # stop service with desired tasks
+    # elif 'stop' == cloudwatchvalue:
+    elif stop_task == s3_bucket_key:
         stopcontainer(service_list, cluster_name)
         print(f"Stop stopcontainer com sucesso")
 
